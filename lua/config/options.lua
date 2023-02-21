ed612f114cc032db53989dd1b8e7d8f58fdfc53e
Ronald Record <github@ronrecord.com>
2023-02-21 22:50:20 +0000

    First round of customizations

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,23 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+-- Set leader to , rather than space
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- Asciiville colorscheme style
+-- Select one of "oceanic", "deep ocean", "palenight", "lighter", "darker"
+-- vim.g.asciiville_style = "deep ocean"
+
+-- disable some extension providers
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+
+-- disable some fanzy UI stuff when run in Neovide
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_floating_blur = 0
+  vim.g.neovide_floating_opacity = 90
+end
